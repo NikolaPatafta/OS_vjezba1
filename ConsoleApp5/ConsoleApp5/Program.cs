8b33f453ec694e88210dae56091ee41dc3ebc4eb
NikolaPatafta <44970174+NikolaPatafta@users.noreply.github.com>
2019-04-06 15:56:07 +0200

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; 
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            // Uzimamo direktorij iz odredjenog diska
+            DirectoryInfo[] cDirs = new DirectoryInfo(@"c:\").GetDirectories();
+
+            // pisemo na odredeni direktorij
+            using (StreamWriter sw = new StreamWriter("CDriveDirs.txt"))
+            {
+                foreach (DirectoryInfo dir in cDirs)
+                {
+                    sw.WriteLine(dir.Name);
+
+                }
+            }
+
+            // Citamo svaku liniju odredjene datoteke
+            string line = "";
+            using (StreamReader sr = new StreamReader("CDriveDirs.txt"))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
+
+
+
